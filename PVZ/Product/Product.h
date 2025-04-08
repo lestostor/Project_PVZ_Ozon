5cb02067034c4740a3098fbc0f9d0dc157844412
Пользователь <olesyabogdaniha2015@gmail.com>
2025-04-08 19:56:27 +0300

    fixed comment

--- PVZ/Product/Product.h
+++ PVZ/Product/Product.h
@@ -7,7 +7,7 @@ class Product {
     int _code;
     int _price;
     bool _prepay;
-    int _age_limit = 0;  //0 - without limit
+    int _age_limit = 0;  //  0 - without limit
     bool _returnability = true;
     Date _date;
 };
