1f3f166c180541d0be49fd171b32a7ecc3ec9803
Пользователь <olesyabogdaniha2015@gmail.com>
2025-04-09 19:35:53 +0300

    restored TVector

---
+++ TVector/TVector/TVector.h
@@ -0,0 +1,127 @@
+#pragma once
+#include <iterator>
+#include <climits>
+
+#define STEP_OF_CAPACITY 15
+#define DELETED INT_MAX
+
+enum Status { Busy, Deleted, Empty };
+template <class T>
+
+class TVector {
+	T* _vec;
+	Status* _status;
+	size_t _size;
+	size_t _capacity;
+public:
+	//constructors
+	TVector();
+	TVector(int);  //empty vector
+	TVector(std::initializer_list<T>);  //convert list to vector
+	TVector(const TVector<T>&);  //copy
+	~TVector();  //destructor
+
+	//getters
+	inline size_t size() const noexcept {
+		return this->_size;
+	}
+
+	inline size_t capacity() const noexcept {
+		return this->_capacity;
+	}
+
+	inline T* data() const noexcept {
+		return _vec;
+	}
+
+	inline T* begin() const noexcept {
+		return _vec;
+	}
+
+	inline T* end() const noexcept {
+		return _vec + _size;
+	}
+
+	inline T& front() const noexcept {
+		return *_vec;
+	}
+
+	inline T& back() const noexcept {
+		return *(_vec + _size - 1);
+	}
+
+	inline bool is_empty() const noexcept {
+		for (int i = 0; i < _size; i++) {
+			if (_status[i] == Status::Busy) return false;
+		}
+		return true;
+	}
+
+	void clear();
+};
+
+template <class T>
+TVector<T>::TVector() {
+	_size = 0;
+	_capacity = 0;
+	_vec = nullptr;
+	_status = nullptr;
+}
+
+template <class T>
+TVector<T>::TVector(int size) {
+	_size = size;
+	_capacity = (_size / STEP_OF_CAPACITY + 1) * STEP_OF_CAPACITY;
+	_vec = new T[size];
+	_status = new Status[_capacity];
+
+	for (int i = 0; i < _capacity; i++)
+		_status[i] = Status::Empty;
+}
+
+template <class T>
+TVector<T>::TVector(std::initializer_list<T> mass) {
+	_size = mass.size();
+	_capacity = (_size / STEP_OF_CAPACITY + 1) * STEP_OF_CAPACITY;
+	_vec = new T[_size];
+	_status = new Status[_capacity];
+
+	auto it = mass.begin();
+	for (int i = 0; i < _capacity; i++, it++) {
+		if (i < _size) {
+			_vec[i] = *it;
+			_status[i] = Status::Busy;
+		}
+		else _status[i] = Status::Empty;
+	}
+}
+
+template <class T>
+TVector<T>::TVector(const TVector& other) {
+	_size = other._size;
+	_capacity = _size / STEP_OF_CAPACITY * STEP_OF_CAPACITY + STEP_OF_CAPACITY;
+	_vec = new T[other._size];
+	_status = new Status[_capacity];
+
+	for (int i = 0; i < _capacity; i++) {
+		if (i < other._size) {
+			_vec[i] = other._vec[i];
+			_status[i] = Status::Busy;
+		}
+		else _status[i] = Status::Empty;
+	}
+}
+
+template <class T>
+void TVector<T>::clear() {
+	for (int i = 0; i < _size; i++) {
+		_vec[i] = DELETED;
+		_status[i] = Status::Deleted;
+	}
+}
+
+template <class T>
+TVector<T>::~TVector() {
+	delete[] _vec;
+	delete[] _status;
+}
---
+++ TVector/Test_TVector/main.cpp
@@ -0,0 +1,224 @@
+#include "TVector.h"
+#include <iostream>
+#include <cstdlib>
+#include <iterator>
+#include <windows.h>
+
+void set_color(int text_color, int bg_color) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, (bg_color << 4) | text_color);
+}
+
+namespace TestSystem {
+    int count_success = 0, count_failed = 0;
+
+    void start_test(bool(*test)(), const char* name_of_test) {
+        set_color(2, 0);
+        std::cout << "[ RUN      ]";
+        set_color(7, 0);
+        std::cout << name_of_test << std::endl;
+
+        bool status = test();
+
+        if (status == true) {
+            set_color(2, 0);
+            std::cout << "[       OK ]" << std::endl;
+            count_success++;
+        }
+        else {
+            set_color(4, 0);
+            std::cout << "[  FAILED  ]" << std::endl;
+            count_failed++;
+        }
+        set_color(7, 0);
+    }
+
+    template <class T>
+    bool check(const T& expected, const T& actual) {
+        if (expected == actual) {
+            return true;
+        }
+        else {
+            std::cerr << "Expected result is " << expected << ", but actual is " << actual << "." << std::endl;
+            return false;
+        }
+    }
+
+    void print_init_info() {
+        set_color(2, 0);
+        std::cout << "[==========] " << std::endl;
+        set_color(7, 0);
+    }
+
+    void print_final_info() {
+        set_color(2, 0);
+        std::cout << "[==========] ";
+        set_color(7, 0);
+        std::cout << count_success + count_failed << " test" << (count_success + count_failed > 1 ? "s" : "") << " ran." << std::endl;
+        set_color(2, 0);
+        std::cout << "[  PASSED  ] ";
+        set_color(7, 0);
+        std::cout << count_success << " test" << (count_success > 1 ? "s" : "") << std::endl;
+        if (count_failed > 0) {
+            set_color(4, 0);
+            std::cout << "[  FAILED  ] ";
+            set_color(7, 0);
+            std::cout << count_failed << " test" << (count_failed > 1 ? "s." : ".") << std::endl;
+        }
+    }
+};
+
+bool test1_try_create_empty_vector() {
+    bool expected_result = true;
+    bool actual_result = true;
+    try {
+        TVector<int> vec;
+    }
+    catch (const std::exception& ex) {
+        actual_result = false;
+        throw std::logic_error("Vector wasn't created\n");
+    }
+
+    return TestSystem::check(expected_result, actual_result);
+}
+
+bool test2_try_create_vector() {
+    bool expected_result = true;
+    bool actual_result = true;
+    try {
+        TVector<int> vec(5);
+    }
+    catch (const std::exception& ex) {
+        actual_result = false;
+        throw std::logic_error("Vector wasn't created\n");
+    }
+
+    return TestSystem::check(expected_result, actual_result);
+}
+
+bool test3_try_convert_mass_to_vector() {
+    bool expected_result = true;
+    bool actual_result = true;
+    try {
+        TVector<int> vec({ 1,2,3,4,5 });
+    }
+    catch (const std::exception& ex) {
+        actual_result = false;
+        throw std::logic_error("Vector wasn't created\n");
+    }
+
+    return TestSystem::check(expected_result, actual_result);
+}
+
+bool test4_try_copy_vector() {
+    bool expected_result = true;
+    bool actual_result = true;
+    try {
+        TVector<int> vec1(5);
+        TVector<int> vec2(vec1);
+    }
+    catch (const std::exception& ex) {
+        actual_result = false;
+        throw std::logic_error("Vector wasn't created\n");
+    }
+
+    return TestSystem::check(expected_result, actual_result);
+}
+
+bool test5_try_get_size() {
+    size_t expected_result = 5;
+    TVector<int> vec(5);
+    size_t actual_result = vec.size();
+
+    return TestSystem::check(expected_result, actual_result);
+}
+
+bool test6_try_get_capacity() {
+    size_t expected_result = 15;
+    TVector<int> vec(5);
+    size_t actual_result = vec.capacity();
+
+    return TestSystem::check(expected_result, actual_result);
+}
+
+bool test7_try_get_data() {
+    int* expected_result = new int[5];
+    for (int i = 0; i < 5; i++) expected_result[i] = i + 1;
+    TVector<int> vec({ 1,2,3,4,5 });
+    int* actual_result = vec.data();
+
+    for (int i = 0; i < 5; i++) {
+        if (!TestSystem::check(expected_result[i], actual_result[i])) return false;
+    }
+    return true;
+}
+
+bool test8_try_get_iterator_begin() {
+    TVector<int> vec({ 1,2,3,4,5 });
+    int mass[5] = { 1,2,3,4,5 };
+    auto expected_result = std::begin(mass);
+    auto actual_result = vec.begin();
+
+    return TestSystem::check(*expected_result, *actual_result);
+}
+
+bool test9_try_get_iterator_end() {
+    TVector<int> vec({ 1,2,3,4,5 });
+    int mass[5] = { 1,2,3,4,5 };
+    auto expected_result = std::end(mass) - 1;
+    auto actual_result = vec.end() - 1;
+
+    return TestSystem::check(*expected_result, *actual_result);
+}
+
+bool test10_try_get_front() {
+    TVector<int> vec({ 1,2,3,4,5 });
+    int expected_result = 1;
+    int actual_result = vec.front();
+
+    return TestSystem::check(expected_result, actual_result);
+}
+
+bool test11_try_get_back() {
+    TVector<int> vec({ 1,2,3,4,5 });
+    int expected_result = 5;
+    int actual_result = vec.back();
+
+    return TestSystem::check(expected_result, actual_result);
+}
+
+bool test12_try_check_is_empty_if_full() {
+    TVector<int> vec({ 1,2,3,4,5 });
+    bool expected_result = false;
+    bool actual_result = vec.is_empty();
+
+    return TestSystem::check(expected_result, actual_result);
+}
+
+bool test13_try_check_is_empty_if_empty() {
+    TVector<int> vec;
+    bool expected_result = true;
+    bool actual_result = vec.is_empty();
+
+    return TestSystem::check(expected_result, actual_result);
+}
+
+int main() {
+    TestSystem::start_test(test1_try_create_empty_vector, "test1_try_create_empty_vector");
+    TestSystem::start_test(test2_try_create_vector, "test2_try_create_vector");
+    TestSystem::start_test(test3_try_convert_mass_to_vector, "test3_try_convert_mass_to_vector");
+    TestSystem::start_test(test4_try_copy_vector, "test4_try_copy_vector");
+    TestSystem::start_test(test5_try_get_size, "test5_try_get_size");
+    TestSystem::start_test(test6_try_get_capacity, "test6_try_get_capacity");
+    TestSystem::start_test(test7_try_get_data, "test7_try_get_data");
+    TestSystem::start_test(test8_try_get_iterator_begin, "test8_try_get_iterator_begin");
+    TestSystem::start_test(test9_try_get_iterator_end, "test9_try_get_iterator_end");
+    TestSystem::start_test(test10_try_get_front, "test10_try_get_front");
+    TestSystem::start_test(test11_try_get_back, "test11_try_get_back");
+    TestSystem::start_test(test12_try_check_is_empty_if_full, "test12_try_check_is_empty_if_full");
+    TestSystem::start_test(test13_try_check_is_empty_if_empty, "test13_try_check_is_empty_if_empty");
+
+    TestSystem::print_final_info();
+
+    return 0;
+}
